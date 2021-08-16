0cdf0f7a1a040ef2408b319d37333c101ff08524
hienhnh246 <hien.hnh246@gmail.com>
2021-08-16 14:27:37 +0700

    Edit bubble sort algorithm

--- bubble-sort-algo.c
+++ bubble-sort-algo.c
@@ -4,6 +4,8 @@ void bubbleSort(int array[], int size)
 {
 	for (int step = 0; step < size - 1; ++step)
 	{
+		int swapped = 0;
+
 		for (int i = 0; i < size - step - 1; ++i)
 		{
 			if (array[i] > array[i + 1])
@@ -11,8 +13,12 @@ void bubbleSort(int array[], int size)
 				int temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
+				swapped = 1;
 			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
 
